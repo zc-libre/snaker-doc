9faa8fb287eccfd5487db4a7c34dc55b3427e80e
zc-libre <zc150622@gmail.com>
2021-01-21 01:05:14 +0800

    文档完善

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,51 +1,120 @@
 module.exports = {
-  base: "/snaker-docs/", //目录根地址，应与Github仓库名字相同
-  title: "snaker-flow", // 显示在左上角的网页名称以及首页在浏览器标签显示的title名称
-  description: "snakerflow", // meta 中的描述文字，用于SEO
-  head: [
-    [
-      "link",
-      { rel: "icon", href: "/favicon.ico" }, //浏览器的标签栏的网页图标,基地址/docs/.vuepress/public
+    // 插件
+    plugins: {
+        '@vuepress/back-to-top': {}
+    },
+    // 头部
+    head: [
+        ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }],
+        ['link', { rel: 'icon', href: '/logo/small.png' }]
     ],
-  ],
-    markdown: {
-    lineNumbers: true, //是否在每个代码块的左侧显示行号
-  },
-   themeConfig: {
-   /* nav: [
-      //链接页面链接的根地址为/docs
-      { text: "思路", link: "/pages/flow.md" },
-      { text: "创建Github仓库", link: "/pages/Github.md" },
-      { text: "配置VuePress", link: "/pages/VuePress.md" },
-      { text: "TravisCI生成和发布", link: "/pages/TravisCI.md" },
-      { text: "博客", link: "https://tsanfer.xyz" },
-    ],*/
-	
-	sidebarDepth: 2, //侧边栏深度
-    sidebar: [
-      ["/pages/4.9 任务参与者.md", "4.9 任务参与者"]
-    ],
-	 // 假定是 GitHub. 同时也可以是一个完整的 GitLab URL
-    repo: "zc-libre/snaker-docs",
-    // 自定义仓库链接文字。默认从 `themeConfig.repo` 中自动推断为
-    // "GitHub"/"GitLab"/"Bitbucket" 其中之一，或是 "Source"。
-    repoLabel: "Github",
-    // 以下为可选的编辑链接选项
-    // 假如文档不是放在仓库的根目录下：
-    docsDir: "docs/pages",
-    // 假如文档放在一个特定的分支下：
-    docsBranch: "main",
-    // 默认是 false, 设置为 true 来启用
-    editLinks: true,
-    // 默认为 "Edit this page"
-    editLinkText: "在 Github 上编辑此页",
-	smoothScroll: true, //页面滚动效果
-    lastUpdated: "最后更新", // string | boolean
-	
-  },
-   plugins: [
-    "@vuepress/medium-zoom", //zooming images like Medium（页面弹框居中显示）
-    "@vuepress/nprogress", //网页加载进度条
-    "@vuepress/plugin-back-to-top", //返回页面顶部按钮
-  ]
-};
+    // 网站标题及描述
+    theme: 'reco',
+    title: 'EL-ADMIN',
+    description: '一个简单且易上手的 Spring boot 后台管理框架',
+    // 主题配置
+    themeConfig: {
+        // 提取markdown中h2 和 h3 标题，显示在侧边栏上。
+        sidebarDepth: 2,
+        // 文档更新时间
+        lastUpdated: '更新时间',
+        // logo
+        subSidebar: 'auto',
+        logo: '/logo/small.png',
+        author: 'ZhengJie',
+        // 备案
+        record: '浙ICP备18005431号',
+        recordLink: 'https://beian.miit.gov.cn/#/Integrated/index',
+        // 项目开始时间，只填写年份
+        startYear: '2018',
+        nav: [
+            { text: '项目指南', link: '/guide/', icon: 'reco-document'},
+            { text: '常见问题', link: '/problem/', icon: 'reco-faq'},
+            { text: '更新日志', link: '/version/V2.6/', icon: 'reco-date'},
+            { text: '捐赠支持', link: '/donation/', icon: 'reco-account'},
+            { text: '体验地址', link: 'https://el-admin.xin', icon: 'reco-other'},
+            // 下拉列表
+            {
+                text: 'Github',
+                items: [
+                    {
+                        text: '前端源码',
+                        link: 'https://github.com/elunez/eladmin-web'
+                    },
+                    {
+                        text: '后端源码',
+                        link: 'https://github.com/elunez/eladmin'
+                    },
+                ],
+                icon: 'reco-github'
+            },
+            {
+                text: 'Gitee',
+                items: [
+                    {
+                        text: '前端源码',
+                        link: 'https://gitee.com/elunez/eladmin-web'
+                    },
+                    {
+                        text: '后端源码',
+                        link: 'https://gitee.com/elunez/eladmin'
+                    }
+                ],
+                icon: 'reco-mayun'
+            }
+        ],
+        sidebar: {
+            '/guide/': [
+                {
+                    title: '指南',
+                    collapsable: false,
+                    children: [
+                        '/guide/',
+                        '/guide/quick-start',
+                        '/guide/4.9 任务参与者'
+                    ]
+                },
+                {
+                    title: '其它',
+                    collapsable: false,
+                    children: [
+                        '/guide/gxdm',
+                        '/guide/bqsm'
+                    ]
+                },
+                {
+                    title: '鸣谢',
+                    collapsable: false,
+                    children: [
+                        '/guide/mx'
+                    ]
+                }
+            ],
+            '/donation/': [
+                '/donation/',
+            ],
+            '/problem/': [
+                '/problem/',
+            ],
+            '/version/': [
+                {
+                    title: '更新日志',
+                    collapsable: false,
+                    children: [
+                        'V2.6',
+                        'V2.5',
+                        'V2.4'
+                    ]
+                }
+            ]
+        },
+        // 假如你的文档仓库和项目本身不在一个仓库：
+        docsRepo: 'elunez/eladmin-doc',
+        // 假如文档不是放在仓库的根目录下：
+        docsDir: 'docs',
+        // 默认是 false, 设置为 true 来启用
+        editLinks: true,
+        // 默认为 "Edit this page"
+        editLinkText: '在 GitHub 上编辑此页！'
+    }
+}
